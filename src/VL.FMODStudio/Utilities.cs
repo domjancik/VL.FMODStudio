3f9238da2a667b8cce0787af9cac9f43aca84cff
ravazquez <ravazquez@gmail.com>
2020-10-31 18:13:27 +0000

    Add Driver Info and several system configuration inputs

--- src/VL.FMODStudio/Utilities.cs
+++ src/VL.FMODStudio/Utilities.cs
@@ -15,7 +15,7 @@ namespace VL.FMODStudio
             }
         }
 
-        public static FMOD.Studio.System initStudioSystem(bool enableLiveUpdate = false)
+        /*public static FMOD.Studio.System initStudioSystem(bool enableLiveUpdate = false, OUTPUTTYPE outputType = OUTPUTTYPE.AUTODETECT, int driverIndex = 0)
         {
             var initFlags = enableLiveUpdate ? FMOD.Studio.INITFLAGS.LIVEUPDATE : FMOD.Studio.INITFLAGS.NORMAL;
 
@@ -23,6 +23,30 @@ namespace VL.FMODStudio
             // Create the Studio System object.
             FMOD.Studio.System system;
             checkResult(FMOD.Studio.System.create(out system));
+            FMOD.System coreSystem;
+            system.getCoreSystem(out coreSystem);
+            coreSystem.setOutput(outputType);
+            coreSystem.setDriver(driverIndex);
+            // Initialize FMOD Studio, which will also initialize FMOD Core
+            checkResult(system.initialize(512, initFlags, FMOD.INITFLAGS.NORMAL, IntPtr.Zero));
+
+            return system;
+        }*/
+
+        public static FMOD.Studio.System initStudioSystem(bool enableLiveUpdate = false, OUTPUTTYPE outputType = OUTPUTTYPE.AUTODETECT, int driverIndex = 0, int sampleRate = 48000, SPEAKERMODE speakerMode = SPEAKERMODE.DEFAULT, int numRawSpeakers = 0, uint bufferLength = 1024, int numBuffers = 4)
+        {
+            var initFlags = enableLiveUpdate ? FMOD.Studio.INITFLAGS.LIVEUPDATE : FMOD.Studio.INITFLAGS.NORMAL;
+
+            Console.WriteLine("FMODSystem: Initializing");
+            // Create the Studio System object.
+            FMOD.Studio.System system;
+            checkResult(FMOD.Studio.System.create(out system));
+            FMOD.System coreSystem;
+            system.getCoreSystem(out coreSystem);
+            coreSystem.setOutput(outputType);
+            coreSystem.setDriver(driverIndex);
+            coreSystem.setSoftwareFormat(sampleRate, speakerMode, numRawSpeakers);
+            coreSystem.setDSPBufferSize(bufferLength, numBuffers);
             // Initialize FMOD Studio, which will also initialize FMOD Core
             checkResult(system.initialize(512, initFlags, FMOD.INITFLAGS.NORMAL, IntPtr.Zero));
 
